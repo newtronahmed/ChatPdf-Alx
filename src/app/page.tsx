28bb627b02c1572c7b6ca9081291f9cb3777c5f7
henry-codex <httetteh@st.ug.edu.gh>
2024-05-21 21:26:23 +0000

    Created auth with Clerk, current route is private.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,10 @@
+import { Button } from "@/components/ui/button";
+
 export default function Home() {
-  return <h1 className = "text-blue-800"> hello alx</h1>
-  
+  return (
+    <div>
+      <h1 className="text-4xl font-bold">Welcome to ALX Project</h1>
+      <Button>Click me</Button>
+    </div>
+  );
 }
