6c79e4bfefd627f3affc14862a7367b44482ab6b
neutronahmed <bazubairu@st.ug.edu.gh>
2024-05-25 13:56:30 +0000

    successful upload to s3

--- src/components/ui/FileUpload.tsx
+++ src/components/ui/FileUpload.tsx
@@ -1,10 +1,10 @@
 "use client";
 import { uploadToS3 } from "@/lib/s3";
-import { useMutation } from "@tanstack/react-query";
+// import { useMutation } from "@tanstack/react-query";
 import { Inbox, Loader2 } from "lucide-react";
 import React from "react";
 import { useDropzone } from "react-dropzone";
-import axios from "axios";
+// import axios from "axios";
 import { toast } from "react-hot-toast";
 import { useRouter } from "next/navigation";
 
@@ -13,21 +13,21 @@ import { useRouter } from "next/navigation";
 const FileUpload = () => {
   const router = useRouter();
   const [uploading, setUploading] = React.useState(false);
-  const { mutate, isLoading } = useMutation({
-    mutationFn: async ({
-      file_key,
-      file_name,
-    }: {
-      file_key: string;
-      file_name: string;
-    }) => {
-      const response = await axios.post("/api/create-chat", {
-        file_key,
-        file_name,
-      });
-      return response.data;
-    },
-  });
+//   const { mutate, isLoading } = useMutation({
+//     mutationFn: async ({
+//       file_key,
+//       file_name,
+//     }: {
+//       file_key: string;
+//       file_name: string;
+//     }) => {
+//       const response = await axios.post("/api/create-chat", {
+//         file_key,
+//         file_name,
+//       });
+//       return response.data;
+//     },
+//   });
 
   const { getRootProps, getInputProps } = useDropzone({
     accept: { "application/pdf": [".pdf"] },
@@ -48,16 +48,17 @@ const FileUpload = () => {
           toast.error("Something went wrong");
           return;
         }
-        mutate(data, {
-          onSuccess: ({ chat_id }) => {
-            toast.success("Chat created!");
-            router.push(`/chat/${chat_id}`);
-          },
-          onError: (err) => {
-            toast.error("Error creating chat");
-            console.error(err);
-          },
-        });
+        toast.success('Upload successful')
+        // mutate(data, {
+        //   onSuccess: ({ chat_id }) => {
+        //     toast.success("Chat created!");
+        //     router.push(`/chat/${chat_id}`);
+        //   },
+        //   onError: (err) => {
+        //     toast.error("Error creating chat");
+        //     console.error(err);
+        //   },
+        // });
       } catch (error) {
         console.log(error);
       } finally {
@@ -74,7 +75,7 @@ const FileUpload = () => {
         })}
       >
         <input {...getInputProps()} />
-        {uploading || isLoading ? (
+        {uploading  ? (
           <>
             {/* loading state */}
             <Loader2 className="h-10 w-10 text-blue-500 animate-spin" />
