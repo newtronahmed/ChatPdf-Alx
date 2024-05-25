a6ef284004fcd2f577e5c9a2844487068618a83b
neutronahmed <bazubairu@st.ug.edu.gh>
2024-05-25 12:39:14 +0000

    drizzle changes

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -1,13 +1,20 @@
-import type { Config } from "drizzle-kit";
+import  { Config, defineConfig } from "drizzle-kit";
 import * as dotenv from "dotenv";
-dotenv.config({ path: ".env" });
+dotenv.config({ path: ".env.local" });
 
-export default {
-  driver: "pg",
-  schema: "./src/lib/db/schema.ts",
-  dbCredentials: {
-    connectionString: process.env.DATABASE_URL!,
-  },
-} satisfies Config;
+// export default {
+//   dialect :"postgresql",
+//   schema: "./src/lib/db/schema.ts",
+//   dbCredentials: {
+//     url: process.env.DATABASE_URL!,
+//   },
+// } satisfies Config;
+export default defineConfig({
+    dialect :"postgresql",
+    schema: "./src/lib/db/schema.ts",
+    dbCredentials: {
+      url: process.env.DATABASE_URL!,
+    },
+})
 
 // npx drizzle-kit push:pg
