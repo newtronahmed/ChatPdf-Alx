2804c9342a1e8fda801909516e4b250da3f3cbee
henry-codex <httetteh@st.ug.edu.gh>
2024-05-25 21:28:36 +0000

    setup openai api

--- src/lib/pinecone.ts
+++ src/lib/pinecone.ts
@@ -1,7 +1,10 @@
 import { Pinecone, PineconeRecord } from "@pinecone-database/pinecone";
 import { downloadFromS3 } from "./s3-server";
 import { PDFLoader } from "langchain/document_loaders/fs/pdf";
-// import md5 from "md5";
+import { Document, RecursiveCharacterTextSplitter } from "@pinecone-database/doc-splitter";
+import md5 from "md5";
+import { getEmbeddings } from "./embeddings"; // Import the getEmbeddings function from "./embeddings".
+
 // import {
 //   Document,
 //   RecursiveCharacterTextSplitter,
@@ -35,6 +38,55 @@ export async function loadS3IntoPinecone(fileKey: string) {
   return pages
 
   // 2. split and segment the pdf
+const documents = await Promise.all(pages.map(prepareDocument));
+
+
+// 3. vectorise and embed individual documents
+}
+
+async function embedDocument(doc: Document) {
+  try {
+    const embeddings = await getEmbeddings(doc.pageContent);
+    const hash = md5(doc.pageContent);
+
+    return {
+      id: hash,
+      values: embeddings,
+      metadata: {
+        text: doc.metadata.text,
+        pageNumber: doc.metadata.pageNumber,
+      },
+    } as PineconeRecord;
+  } catch (error) {
+    console.log("error embedding document", error);
+    throw error;
+  }
+}
+
+export const truncateStringByBytes = (str: string, bytes: number) => {
+  const enc = new TextEncoder();
+  return new TextDecoder("utf-8").decode(enc.encode(str).slice(0, bytes));
+}
+
+async function prepareDocument(page: PDFPage) {
+  let { pageContent, metadata } = page;
+  pageContent = pageContent.replace(/\n/g, "");
+  // split the docs
+  const splitter = new RecursiveCharacterTextSplitter();
+  const docs = await splitter.splitDocuments([
+    new Document({
+      pageContent,
+      metadata: {
+        pageNumber: metadata.loc.pageNumber,
+        text: truncateStringByBytes(pageContent, 36000),
+      },
+    }),
+  ]);
+  return docs;
+
+}
+
+
 //   const documents = await Promise.all(pages.map(prepareDocument));
 
 //   // 3. vectorise and embed individual documents
@@ -49,7 +101,7 @@ export async function loadS3IntoPinecone(fileKey: string) {
 //   await namespace.upsert(vectors);
 
 //   return documents[0];
-}
+
 
 // async function embedDocument(doc: Document) {
 //   try {
