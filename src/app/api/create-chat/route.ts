0d17463009f04fdd26b143edb1b0fc7c5a307bbf
henry-codex <httetteh@st.ug.edu.gh>
2024-05-26 08:46:32 +0000

    updated created chat feature

--- src/app/api/create-chat/route.ts
+++ src/app/api/create-chat/route.ts
@@ -7,35 +7,38 @@ import { NextResponse } from "next/server";
 
 // /api/create-chat
 export async function POST(req: Request, res: Response) {
-    const { userId} = await auth();
-    if (!userId) {
-        return NextResponse.json({ error: "unauthorized" }, { status: 401 });
-    }
+  const { userId } = await auth();
+  if (!userId) {
+    return NextResponse.json({ error: "unauthorized" }, { status: 401 });
+  }
   try {
     const body = await req.json();
     const { file_key, file_name } = body;
     console.log(file_key, file_name);
     await loadS3IntoPinecone(file_key);
     const chat_id = await db
-    .insert(chats) 
-    .values({
-       fileKey: file_key,
-       pdfName: file_name,
+      .insert(chats)
+      .values({
+        fileKey: file_key,
+        pdfName: file_name,
         pdfUrl: getS3Url(file_key),
         userId,
-    })
-    .returning({
-        insertId: chats.id,
-    });
+      })
+      .returning({
+        insertedId: chats.id,
+      });
 
     return NextResponse.json(
-        {
-            chat_id: chat_id[0].insertId,
-        },
-        { status: 200 }
+      {
+        chat_id: chat_id[0].insertedId,
+      },
+      { status: 200 }
     );
-    } catch (error) {
-        console.error(error);
-        return NextResponse.json({ error: "internal server error" }, { status: 500 });
-    }
+  } catch (error) {
+    console.error(error);
+    return NextResponse.json(
+      { error: "internal server error" },
+      { status: 500 }
+    );
+  }
 }
