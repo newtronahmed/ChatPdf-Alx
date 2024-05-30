58dac299307d16f72de5d86f319a4a1336777136
henry-codex <httetteh@st.ug.edu.gh>
2024-05-30 18:39:25 +0000

    fininshed with the chat context feature.

--- src/lib/pinecone.ts
+++ src/lib/pinecone.ts
@@ -1,6 +1,5 @@
 import { Pinecone, PineconeRecord } from "@pinecone-database/pinecone";
 import { downloadFromS3 } from "./s3-server";
-//import { PDFLoader } from "langchain/document_loaders/fs/pdf";
 import { PDFLoader } from "@langchain/community/document_loaders/fs/pdf";
 import md5 from "md5";
 import {
@@ -45,7 +44,7 @@ const vectors = await Promise.all(documents.flat().map(embedDocument));
 
 //4. upload to pinecone
 const client = await getPineconeClient();
-const pineconeIndex = await client.index("chatpdf-alx");
+const pineconeIndex = await client.index("chat-pdf-alx-2");
 const namespace = pineconeIndex.namespace(convertToAscii(fileKey));
 
 //const client = await getPineconeClient();
